39548c510302593a0fedfd35e1ee3c5c45ac62b5
devjohurul <jaharul15-13653@diu.edu.bd>
2026-07-09 15:32:04 +0600

    inital project

---
+++ src/main/java/com/johurulislam/educationmanagementsystemwms/EducationManagementSystemWmsApplication.java
@@ -0,0 +1,13 @@
+package com.johurulislam.educationmanagementsystemwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationManagementSystemWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationManagementSystemWmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johurulislam/educationmanagementsystemwms/EducationManagementSystemWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johurulislam.educationmanagementsystemwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationManagementSystemWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
